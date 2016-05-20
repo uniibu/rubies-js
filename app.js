27dfecbf0b88643e18f1f591af46f93f8bb1f08f
unibtc <unibtc@gmail.com>
2016-05-21 06:10:32 +0800

    initial commit

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ var rubies = module.exports;
 rubies.Address = require('./lib/address');
 rubies.PrivateKey = require('./lib/privatekey');
 rubies.PublicKey = require('./lib/publickey');
+rubies.crypto = {};
+rubies.crypto.BN = require('./lib/crypto/bn');
+
+rubies.crypto.Hash = require('./lib/crypto/hash');
+rubies.crypto.Random = require('./lib/crypto/random');
+rubies.crypto.Point = require('./lib/crypto/point');
+
+var value = new Buffer('correct horse battery staple');
+var hash = rubies.crypto.Hash.sha256(value);
+var bn = rubies.crypto.BN.fromBuffer(hash);
+
+var address = new rubies.PrivateKey(bn,'testnet').toAddress();
+
+console.log(address);
