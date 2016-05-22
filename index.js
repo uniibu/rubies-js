65659628c62c67a79852c8a834875f3bd0428769
uniibu <unibtc@gmail.com>
2016-05-22 21:34:40 +0800

    Update and rename app.js to index.js

--- index.js
+++ index.js
@@ -11,9 +11,3 @@ rubies.HdPublicKey = require('./lib/hdpublickey');
 rubies.crypto.Hash = require('./lib/crypto/hash');
 rubies.crypto.Random = require('./lib/crypto/random');
 rubies.crypto.Point = require('./lib/crypto/point');
-var HDPrivateKey = rubies.HdPrivateKey;
-var Random = rubies.Random;
-var hdPrivateKey = new HDPrivateKey('livenet',false);
-
-console.log(hdPrivateKey.derive(1));
-console.log(hdPrivateKey.hdPublicKey);
